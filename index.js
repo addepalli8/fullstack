bffb58c3019c5a9b1c98ba051b71069d4d3a5920
addepalli8 <sardhakaddepalli@gmail.com>
2019-02-11 18:13:34 +0530

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+const PORT =process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    res.send({hi:"OMG"});
+});
+console.log(PORT);
+app.listen(PORT);
