19c44dbab5d013edd3ef499b7e82e8507b519f35
addepalli8 <sardhakaddepalli@gmail.com>
2019-02-14 00:35:34 +0530

    last

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,7 @@ passport.use(
 		{
 			clientID: keys.googleclientID,
 			clientSecret: keys.googlesecret,
-			callbackURL: 'https://thawing-reef-28565.herokuapp.com//auth/google/callback'
+			callbackURL: 'https://thawing-reef-28565.herokuapp.com/auth/google/callback'
 		},
 		(accesstoken, refreshtoken, profile, done) => {
 			user.findOne({ googleId: profile.id }).then((olduser) => {
