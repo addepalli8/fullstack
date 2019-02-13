ba769220dbcaddd22eaa77d08ed54c5ffc9ebd91
addepalli8 <sardhakaddepalli@gmail.com>
2019-02-13 23:44:41 +0530

    msg

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,15 @@
-if(process.env.NODE_ENV==="production")
+if(process.env.NODE_ENV==='production')
 {
-	module.exports=require('./prod')
+	module.exports=require('./prod');
 }
-else
-{
-	module.exports=require('./dev')
+else{
+	module.exports=require('./dev');
 
 }
+
+// module.exports = {
+// 	googleclientID: '172505613922-vqh007f1mrnaqqcirvt4b87llqmufcl2.apps.googleusercontent.com',
+// 	googlesecret: 'aJIJehclvBCZ_YCU_tNCc4lZ',
+// 	mongoURI:'mongodb+srv://user:user@cluster0-4nbqb.mongodb.net/test?retryWrites=true',
+// 	cookiekey:'qazwsxedcrfv'
+// };
